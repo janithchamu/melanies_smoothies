963bd2e4317289c07f64515782c7c1683caa7acd
janith <janithchamupathi99@gmail.com>
2025-02-23 19:23:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,6 @@ ingredients_list = st.multiselect(
     ,max_selections=5
 )
 
-time_to_insert = st.button('Submit Order')
 
 if ingredients_list:
     ingredients_string = ''
@@ -38,7 +37,7 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-
+    time_to_insert = st.button('Submit Order')
     st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) values (
